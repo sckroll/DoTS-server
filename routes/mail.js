64269236ff4b252417be7efbd57a4ae5e7ca6a8e
sckroll <kimsc0714@gmail.com>
2020-11-10 18:11:38 +0900

    Fix: mailer를 SendGrid로 변경

--- routes/mail.js
+++ routes/mail.js
@@ -1,13 +1,16 @@
 import { Router } from 'express'
-import nodeMailer from 'nodemailer'
-import smtpTransport from 'nodemailer-smtp-transport'
+// import nodeMailer from 'nodemailer'
+// import smtpTransport from 'nodemailer-smtp-transport'
 import crypto from 'crypto'
 import asyncHandler from 'express-async-handler'
+import sgMail from '@sendgrid/mail'
 import Project from '../models/projects'
 // import mailConfig from '../config/mail'
-import mailConfig from '../../configData/mail'
+// import mailConfig from '../../configData/mail'
+import { apiKey } from '../../configData/mail'
 
 const router = Router()
+sgMail.setApiKey(apiKey)
 
 router.post(
   '/',
@@ -17,7 +20,7 @@ router.post(
     const newMember = req.body.newMember
     const project = req.body.project
 
-    const transporter = nodeMailer.createTransport(smtpTransport(mailConfig))
+    // const transporter = nodeMailer.createTransport(smtpTransport(mailConfig))
     const firstKey = crypto.randomBytes(256).toString('hex').substr(100, 16)
     const secondKey = crypto.randomBytes(256).toString('hex').substr(200, 16)
     const verifyKey = firstKey + secondKey
@@ -47,9 +50,23 @@ router.post(
       { new: true },
     )
 
-    const mailOptions = {
-      from: 'kimsc0714@gmail.com',
+    // const mailOptions = {
+    //   from: 'kimsc0714@gmail.com',
+    //   to: memberEmail,
+    //   subject: `[DoTS 프로젝트 초대] "${project.project_name}" 프로젝트 팀원 초대 메일입니다.`,
+    //   html:
+    //     `<h2>${project.project_name} by ${project.team_name}</h2>` +
+    //     `<p>${project.description}</p>` +
+    //     `<br/><p><strong>${sender.first_name}</strong>(${sender.email})님께서 당신을 ` +
+    //     `<strong>${project.project_name}</strong> 프로젝트에 초대하셨습니다.</p>` +
+    //     `<p><strong>${project.team_name}</strong> 팀의 일원으로서 프로젝트의 목표 달성을 위해 힘써주세요!</p>` +
+    //     '<p>프로젝트의 팀원이 되기를 원하신다면 아래의 링크를 클릭해주세요.</p>' +
+    //     `<br/><a href="${url}/auth?project=${project.project_id}` +
+    //     `&member=${newMember.email}&token=${verifyKey}">인증하기</a>`,
+    // }
+    const message = {
       to: memberEmail,
+      from: 'kimsc0714@gmail.com',
       subject: `[DoTS 프로젝트 초대] "${project.project_name}" 프로젝트 팀원 초대 메일입니다.`,
       html:
         `<h2>${project.project_name} by ${project.team_name}</h2>` +
@@ -63,8 +80,9 @@ router.post(
     }
 
     // 초대 메일 전송
-    const response = await transporter.sendMail(mailOptions)
-    transporter.close()
+    // const response = await transporter.sendMail(mailOptions)
+    // transporter.close()
+    const response = await sgMail.send(message)
     res.send(response)
   }),
 )
